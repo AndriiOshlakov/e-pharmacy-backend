bf1d24ca1f44666e411517c02612a2e6cdd0417b
Andrii <andreyoshlakov18051986@gmail.com>
2026-03-02 09:59:23 +0200

    Initial backend setup

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import helmet from 'helmet';
+
+const app = express();
+const PORT = process.env.PORT ?? 3030;
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
