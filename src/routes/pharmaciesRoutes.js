49eeeccbfcec4bad77d61ea77fb6b2462bf2d22f
Andrii <andreyoshlakov18051986@gmail.com>
2026-03-03 14:27:11 +0200

    fixed route

--- src/routes/pharmaciesRoutes.js
+++ src/routes/pharmaciesRoutes.js
@@ -3,6 +3,6 @@ import { getAllPharmacies } from '../controllers/pharmaciesController.js';
 
 const router = Router();
 
-router.use('/pharmacies', getAllPharmacies);
+router.get('/pharmacies', getAllPharmacies);
 
 export default router;
