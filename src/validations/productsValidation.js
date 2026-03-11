0d29243b7dc532555a30ae344e209333a21cb99e
Andrii <andreyoshlakov18051986@gmail.com>
2026-03-11 18:17:40 +0200

    added alow empty to product validation

--- src/validations/productsValidation.js
+++ src/validations/productsValidation.js
@@ -6,7 +6,9 @@ export const getAllProductsSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(12),
-    category: Joi.string().valid(...CATEGORIES),
+    category: Joi.string()
+      .valid(...CATEGORIES)
+      .allow(''),
     search: Joi.string().allow(''),
   }),
 };
